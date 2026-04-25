096dd1f2433d433ae5e6427fca5fcc10901f36da
Sahil Shaikh <itzmesahilshaikh@gmail.com>
2026-04-25 19:52:14 +0530

    Updated Code

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from flask import Flask, jsonify, render_template_string
 
 app = Flask(__name__)
 
-# HTML template with a blue background
+# HTML template with a green background
 html_template = """
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Green Flask App</title>
+    <title>Green Flask App</title>
     <style>
         body {
             background-color: green;
@@ -28,7 +28,7 @@ html_template = """
 
 @app.route('/')
 def home():
-    return render_template_string(html_template, message="This is Old Version")
+    return render_template_string(html_template, message="This is New update Version")
 
 # Health Check Endpoint
 @app.route('/health')
